9e12a2656646e4a09b84b67cab1818462bfc6e85
anderson.silva <anderson.silva@ndd.tech>
2025-03-23 19:15:56 -0300

    vulkanInstance extensions physicalDevice validationLayer queueFamilies

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-#define GLFW_INCLUDE_VULKAN
+#define VULKAN_HPP_NO_CONSTRUCTORS
+#include <vulkan/vulkan.hpp>
+// #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -6,38 +8,215 @@
 #include <glm/mat4x4.hpp>
 
 #include <iostream>
-#include <vulkan/vulkan.hpp>
+#include <memory>
+#include <format>
+#include <fmt/format.h>
+#include <algorithm>
+#include <optional>
+
+struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+
+    bool isComplete() {
+        return graphicsFamily.has_value();
+    }
+};
 
 class HelloTiangle
 {
 private:
-    void initVulkan() {
+    std::unique_ptr<GLFWwindow, decltype(&::glfwDestroyWindow)> window;
+    static inline constexpr int width = 800;
+    static inline constexpr int height = 600;
+    const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+    static inline constexpr bool enableValidationLayers = [] {
+#ifdef NDEBUG
+    return false;
+#else
+    return true;
+#endif
+    }();
 
+    vk::Instance instance;
+    vk::PhysicalDevice physicalDevice;
+
+    void initVulkan() {
+        createVkInstance();
+        pickPhysicalDevice();
+        createLogicalDevice();
+    }
+
+    void createLogicalDevice() {
+
+    }
+
+    void pickPhysicalDevice() {
+        auto devices = instance.enumeratePhysicalDevices();
+
+        if (devices.size() == 0) {
+            throw std::runtime_error("failed to find GPUs with Vulkan support!");
+        }
+
+        for (const auto& device: devices) {
+            if (isDeviceSuitable(device)) {
+                physicalDevice = device;
+                return;
+            }
+        }
+
+        throw std::runtime_error("failed to find a suitable GPU!");
+    }
+
+    bool isDeviceSuitable(const vk::PhysicalDevice &device) {
+        // auto properties = device.getProperties();
+        // auto features = device.getFeatures();
+        auto indices = findQueueFamilies(device);
+
+        return indices.isComplete();
+    }
+
+    QueueFamilyIndices findQueueFamilies(const vk::PhysicalDevice &device) {
+        auto queueFamilies = device.getQueueFamilyProperties();
+        QueueFamilyIndices indices;
+
+        int i = 0;
+        for (const auto& queueFamily: queueFamilies) {
+            if (queueFamily.queueFlags & vk::QueueFlagBits::eGraphics) {
+                indices.graphicsFamily = i;
+            }
+
+            if (indices.isComplete()) {
+                break;
+            }
+
+            i++;
+        }
+
+        return indices;
+    }
+
+    void createVkInstance() {
+        if (enableValidationLayers && !checkValidationLayersSupport()) {
+            throw std::runtime_error("validation layers requested, but not available!");
+        }
+
+        vk::ApplicationInfo appInfo{
+            .sType = vk::StructureType::eApplicationInfo,
+            .pApplicationName = "Hello Triangle",
+            .applicationVersion = VK_MAKE_VERSION(1,0,0),
+            .pEngineName = "No Engine",
+            .engineVersion = VK_MAKE_VERSION(1,0,0),
+            .apiVersion = VK_API_VERSION_1_4
+        };
+
+        vk::InstanceCreateInfo createInfo{
+            .sType = vk::StructureType::eInstanceCreateInfo,
+            .flags = vk::InstanceCreateFlagBits::eEnumeratePortabilityKHR, // Essa flag é necessaria para o MacOS
+            .pApplicationInfo = &appInfo,
+        };
+
+        uint32_t glfwExtensionCount{};
+        const char** p_glfwExtensions; // TODO: revisar essa alocação, talvez seja um leak
+
+        p_glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        std::vector<const char*> glfwExtensions(p_glfwExtensions, p_glfwExtensions + glfwExtensionCount);
+
+        #ifdef __APPLE__
+            addMacSpecificExtensions(glfwExtensions);
+        #endif
+
+        createInfo.enabledExtensionCount = static_cast<uint32_t>(glfwExtensions.size());
+        createInfo.ppEnabledExtensionNames = glfwExtensions.data();
+
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        } else {
+            createInfo.enabledLayerCount = 0;
+        }
+
+        instance = vk::createInstance(createInfo, nullptr);
+        printGlfwRequiredExtensions(glfwExtensions);
+    }
+
+    void printGlfwRequiredExtensions(const std::vector<const char*> glfwExtensions) {
+        auto extensions = vk::enumerateInstanceExtensionProperties();
+
+        fmt::println("Available extensions:");
+
+        for (const auto& extension : extensions) {
+            const char* extensionName{extension.extensionName.data()};
+            fmt::print("\t {}", extensionName);
+
+            auto glfwRequiresExtension = std::ranges::any_of(glfwExtensions, [&extensionName](const char* value) {
+                return std::strcmp(extensionName, value) == 0;
+            });
+
+            if (glfwRequiresExtension) {
+                fmt::print("*** \t -> GLFW Required\n");
+            } else {
+                fmt::print("\n");
+            }
+        }
+    }
+
+    bool checkValidationLayersSupport() {
+        auto availableLayers = vk::enumerateInstanceLayerProperties();
+
+        for (const auto& layerName: validationLayers) {
+            auto layerFound = std::ranges::any_of(availableLayers, [&layerName](const vk::LayerProperties &layer){
+                return std::strcmp(layerName, layer.layerName.data()) == 0;
+            });
+
+            if (!layerFound) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void addMacSpecificExtensions(std::vector<const char*> &glfwExtensions) {
+        // glfwExtensions.push_back(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME); Só é necessario no Vulkan 1.0
+        glfwExtensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(window.get())) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
+        glfwTerminate();
+    }
 
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window.reset(glfwCreateWindow(width, height, "Vulkan", nullptr, nullptr));
     }
 
 public:
-    HelloTiangle(/* args */) {
-
+    explicit HelloTiangle() :
+        window(std::unique_ptr<GLFWwindow, decltype(&::glfwDestroyWindow)>(nullptr, &::glfwDestroyWindow))
+    {
     }
     ~HelloTiangle() {
 
     }
 
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 };
-
 
 
 int main() {
